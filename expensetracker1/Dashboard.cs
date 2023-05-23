3f1e7d94a24035d44fef47e9c7b0802cd6f0ff74
mayyaribrahim <117400235+mayyaribrahim@users.noreply.github.com>
2023-05-23 03:00:36 +0300

    Add Dashboard

---
+++ expensetracker1/Dashboard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace expensetracker1
+{
+    public partial class btnDashboard : Form
+    {
+        public btnDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void kryptonButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonLabel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void kryptonButton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
