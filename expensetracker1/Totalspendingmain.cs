23ec967a669cd33642adaa8a3782f92d1a0871ae
mayyaribrahim <117400235+mayyaribrahim@users.noreply.github.com>
2023-05-24 21:23:39 +0300

    new form

---
+++ expensetracker1/Totalspendingmain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace expensetracker1
+{
+    public partial class Totalspendingmain : KryptonForm
+    {
+        public Totalspendingmain()
+        {
+            InitializeComponent();
+        }
+
+        private void Totalspendingmain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kryptonPalette1_PalettePaint(object sender, PaletteLayoutEventArgs e)
+        {
+
+        }
+    }
+}
