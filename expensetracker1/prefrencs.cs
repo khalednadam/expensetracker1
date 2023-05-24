381260ae1e2e2918e9a5c3c7b2c2282f2e271a5c
Mohammad ALSEBAI <200513485@ogr.altinbas.edu.tr>
2023-05-24 19:49:52 +0300

    Add prefrences form

---
+++ expensetracker1/prefrencs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+
+namespace expensetracker1
+{
+    public partial class prefrencesForm : KryptonForm
+
+    {
+        public prefrencesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void prefrencs_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrefrences_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
