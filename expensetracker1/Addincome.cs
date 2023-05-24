3f96e1547f0273496a869a9af8ded06da4479d61
mayyaribrahim <117400235+mayyaribrahim@users.noreply.github.com>
2023-05-24 05:03:58 +0300

    New addexpence form and fix

--- expensetracker1/Addincome.cs
+++ expensetracker1/Addincome.cs
@@ -7,10 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ComponentFactory.Krypton.Toolkit;
 namespace expensetracker1
 {
-    public partial class Addincome : Form
+    public partial class Addincome : KryptonForm
     {
         public Addincome()
         {
@@ -43,6 +43,11 @@ namespace expensetracker1
         }
 
         private void txtNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
